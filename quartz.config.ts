4c4347ae00b49b8b81974c0d0b57a94b8908aa12
Aneeth Kumaar <aneethkumaar@icloud.com>
2026-02-24 14:32:23 +0530

    Quartz sync: Feb 24, 2026, 2:32 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "akwastaken.github.io/Measure-Theory-Notes",
+    baseUrl: "iisertvmstudylabs.github.io/StudyMaterial",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
